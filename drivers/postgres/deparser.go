7994a7e44e34dbe6baf780cf68d16a66e1dac5e6
vasconcelosdavi <davi.muniz41@gmail.com>
2022-05-17 17:17:01 -0300

    Add CreateTable in postgres deparser

--- drivers/postgres/deparser.go
+++ drivers/postgres/deparser.go
@@ -8,9 +8,56 @@ import (
 
 type deparser struct { }
 
+func inStringList(stringList []string, needle string) bool {
+	isIn := false
+	for _, s := range stringList {
+		if needle == s {
+			isIn = true
+		}
+	}
+	return isIn
+}
+
+func hasSingleArg(datatype string) bool {
+	singleArgTypes := []string{"char", "varchar", "bit", "varbit", "time", "timestamp"}
+	if inStringList(singleArgTypes, datatype) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func hasDoubleArg(datatype string) bool {
+	doubleArgTypes := []string{"decimal"}
+	if inStringList(doubleArgTypes, datatype) {
+		return true
+	}	else {
+		return false
+	}
+}
+
 func (d *deparser) CreateTable(table *domain.Table) string {
-	// TODO: how to mount this string?
-	return ""
+	string := fmt.Sprintf("CREATE TABLE %s (", table.Name)
+
+	for columnName, column := range table.Columns {
+		string = string + fmt.Sprintf("%s %s", columnName, column.Datatype)
+
+		if hasSingleArg(column.Datatype) {
+			string = string + fmt.Sprintf("(%s)", column.Datatype, column.Parameters["size"])
+		} else if hasDoubleArg(column.Datatype) {
+			string = string + fmt.Sprintf("(%s,%s)", column.Datatype, column.Parameters["precision"], column.Parameters["scale"])
+		}
+
+		if column.IsNotNull {
+			string = string + fmt.Sprintf(" NOT NULL")
+		}
+
+		string = string + fmt.Sprintf(",")
+	}
+
+	string = string[0:len(string) - 2]
+	string = string + fmt.Sprintf(")")
+	return string
 }
 
 func (d *deparser) DropTable(tableName string) string {
