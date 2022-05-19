617297bbba448c4620b6767eee03a55950ac81fc
Guilherme Kowalczuk <guilhermekowalczuk@gmail.com>
2022-05-19 02:30:12 -0300

    Restore generate cmd

--- cmd/generate.go
+++ cmd/generate.go
@@ -10,33 +10,15 @@ import (
 type generate struct{}
 
 func (g *generate) execute(args []string, databaseURL string, driver domain.Driver) error {
-	fileNameOld := args[0]
-	content, err := os.ReadFile(fileNameOld)
+	fileName := args[0]
+	content, err := os.ReadFile(fileName)
 	if err != nil {
-		return fmt.Errorf("could not read from old file: %s", err)
+		return fmt.Errorf("could not read from file: %s", err)
 	}
 
-	oldSchema, err := driver.ParseMigration(string(content))
+	_, err = driver.ParseMigration(string(content))
 	if err != nil {
-		return fmt.Errorf("could not parse migration from old file: %s", err)
-	}
-
-	fileNameNew := args[1]
-	content, err = os.ReadFile(fileNameNew)
-	if err != nil {
-		return fmt.Errorf("could not read from new file: %s", err)
-	}
-
-	newSchema, err := driver.ParseMigration(string(content))
-	if err != nil {
-		return fmt.Errorf("could not parse migration from new file: %s", err)
-	}
-
-	diffQueue := newSchema.Diff(oldSchema)
-	deparser := driver.Deparser()
-	for _, diff := range diffQueue {
-		up := diff.Up(deparser)
-		println(up)
+		return err
 	}
 
 	return nil
