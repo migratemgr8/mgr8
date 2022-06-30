94b63d0ff2c7829e3581e1e451765df06f29f5e5
Monk <kenjiyamane0@gmail.com>
2022-06-30 10:30:49 -0300

    remove duplicate testdata folder

--- testdata/0002_schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users (
-    social_number VARCHAR(9) PRIMARY KEY,
-    nombre VARCHAR(15),
-    phone VARCHAR(11),
-    num DECIMAL(10, 5)
-);
--- testdata/output/0001_1656197671.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users (
-nombre varchar(15),
-num numeric(10,5),
-phone varchar(11),
-social_number varchar(9)
-);
