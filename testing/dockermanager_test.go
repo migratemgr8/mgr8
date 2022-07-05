fb3811c9935d9a9dc348b2503603991cbf6ebfd5
Monk <kenjiyamane0@gmail.com>
2022-07-05 11:27:15 -0300

    expose docker manager and fix testing suite name

--- testing/dockermanager_test.go
+++ testing/dockermanager_test.go
@@ -11,7 +11,7 @@ import (
 
 var _t *testing.T
 
-func TestApplication(t *testing.T) {
+func TestTesting(t *testing.T) {
 	_t = t
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Testing Test Suite")
@@ -19,7 +19,7 @@ func TestApplication(t *testing.T) {
 
 var _ = Describe("Check Command", func() {
 	var (
-		subject *dockerManager
+		subject *DockerManager
 	)
 
 	BeforeSuite(func() {
